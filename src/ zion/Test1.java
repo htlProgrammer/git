4853c3acb0dfb69cb923d3783e65fa3795466901
hutl <1534950128@qq.com>
2019-08-19 15:44:32 +0800

    2222

--- src/ zion/Test1.java
+++ src/ zion/Test1.java
@@ -3,6 +3,7 @@ package zion;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("现在是处于第一步");
-		System.out.println("现在是处于第一步添加一条记录"1111111);
-	}
+		System.out.println("现在是处于第一步添加一条记录");
+		System.out.println("假装解决了冲突");
+	} 
 }
