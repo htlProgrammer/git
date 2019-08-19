d5bd8ea6e0b52dd6902c4b74903783564aed24a4
htlProgrammer <51787955+htlProgrammer@users.noreply.github.com>
2019-08-19 15:09:31 +0800

    Update and rename src/ zion/Test1.java to  src/ zion/Test1.java

--- src/ zion/Test1.java
+++ src/ zion/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("第一次git操作");
 		System.out.println("第一个文件并修改");
-		系统。出。println（“ss”杀杀杀）;
+		系统。出。println（“ss”杀杀杀） ;
 	
 		
 	}
