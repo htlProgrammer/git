cacd196a743ac2b54c666695d81ba4f3948eedce
hutl <1534950128@qq.com>
2019-08-19 14:41:42 +0800

    冲突

--- src/zion/Test1.java
+++ src/zion/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("第一次git操作");
 		System.out.println("第一个文件并修改");
-		System.out.println("");
+		System.out.println("制造第一次冲突");
 		
 	}
 }
