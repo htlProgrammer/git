1b5c78e2835c53534f73485d84b612c61938904a
hutl <1534950128@qq.com>
2019-08-19 14:36:13 +0800

    第二次提交

--- src/zion/Test1.java
+++ src/zion/Test1.java
@@ -3,5 +3,8 @@ package zion;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("第一次git操作");
+		System.out.println("第一个文件并修改");
+		System.out.println("");
+		
 	}
 }
---
+++ src/zion/Test2.java
@@ -0,0 +1,7 @@
+package zion;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("第二个文件，但是啥都没有");
+	}
+}
