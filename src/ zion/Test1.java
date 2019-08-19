af5449d40a49505d35fcf55184483263b981bca7
hutl <1534950128@qq.com>
2019-08-19 15:07:57 +0800

    ml

--- src/ zion/Test1.java
+++ src/ zion/Test1.java
@@ -4,9 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("第一次git操作");
 		System.out.println("第一个文件并修改");
-		System.out.println("制造第一次冲突");
-		System.out.println("制造第一次冲突");
-		
+		System.out.println("现在上面都没有了");
+	
 		
 	}
 }
