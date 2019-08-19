51b7b042f698d6a74be921eee74d5e20255d1e92
htlProgrammer <51787955+htlProgrammer@users.noreply.github.com>
2019-08-19 15:22:50 +0800

    1

--- src/ zion/Test1.java
+++ src/ zion/Test1.java
@@ -3,6 +3,6 @@ package zion;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("现在是处于第一步");
-		System.out.println("现在是处于第一步添加一条记录");
+		System.out.println("现在是处于第一步添加一条记录"1111111);
 	}
 }
